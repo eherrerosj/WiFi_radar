a068c5dc448bbb7aa145a45a11b2eb7529431b47
Enrique <kikexclusive@gmail.com>
2016-03-01 23:46:01 +0100

    Added files via upload

---
+++ radar_foneras/static/rosasvientos.js
@@ -0,0 +1,775 @@
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos1').highcharts({
+      data: {
+        table: 'freq1',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 1. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos2').highcharts({
+      data: {
+        table: 'freq2',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 2. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos3').highcharts({
+      data: {
+        table: 'freq3',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 3. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos4').highcharts({
+      data: {
+        table: 'freq4',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 4. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos5').highcharts({
+      data: {
+        table: 'freq5',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 5. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos6').highcharts({
+      data: {
+        table: 'freq6',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 6. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos7').highcharts({
+      data: {
+        table: 'freq7',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 7. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos8').highcharts({
+      data: {
+        table: 'freq8',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 8. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos9').highcharts({
+      data: {
+        table: 'freq9',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 9. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos10').highcharts({
+      data: {
+        table: 'freq10',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 10. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
+$(function () {
+
+    // Parse the data from an inline table using the Highcharts Data plugin
+    $('#rosavientos11').highcharts({
+      data: {
+        table: 'freq11',
+        startRow: 1,
+        endRow: 14,
+        endColumn: 1
+      },
+      chart: {
+        polar: true,
+        type: 'column'
+      },
+      
+      title: {
+        text: 'FONERA 11. Throughtput medio por canal [kbps]'
+      },
+      
+      subtitle: {
+        text: 'Unidades: kbps. Muestreo: 164 segundos'
+      },
+      
+      pane: {
+        size: '95%'
+      },
+      
+      legend: {
+        enabled: false,
+        reversed: true,
+        align: 'right',
+        verticalAlign: 'top',
+        y: 100,
+        layout: 'vertical'
+      },
+      
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+
+      yAxis: {
+        min: 0,
+        endOnTick: false,
+        showLastLabel: true,
+        title: {
+          text: 'Throughput (kbps)'
+        },
+        labels: {
+          formatter: function () {
+            return this.value + ' kbps';
+          }
+        }
+      },
+      
+      tooltip: {
+        valueSuffix: ' kbps',
+        followPointer: true
+      },
+
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          shadow: false,
+          groupPadding: 0,
+          pointPlacement: 'on'
+        }
+      }
+    });
+});
+
